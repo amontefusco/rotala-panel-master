48d305a8edfe16a69cd97212d114f04f5442e878
Sergio Subrizi <silverdog@libero.it>
2018-05-26 17:07:02 +0200

    Rotala-Panel
    Starting Poin

---
+++ python/rotala.py
@@ -0,0 +1,127 @@
+#!/usr/bin/python
+
+import tornado.ioloop
+import tornado.web
+import tornado.websocket
+import time
+import threading
+import json
+import sys
+
+ws=None
+t_exit=False
+ 
+def counter():
+	i=0
+	z=999
+	r=450
+	while True:
+		if t_exit==True:
+			print "Bye"
+			break
+		i=i+1
+		z=z-1
+		r=r-1
+		time.sleep(1)
+		print i 
+
+		if ws<>None:
+			data = {"target": "display1", "value" : i}
+			data = json.dumps(data)
+			ws.write_message(data)
+
+			data = {"target": "display2", "value" : z}
+			data = json.dumps(data)
+			ws.write_message(data)
+			
+			data = {"target": "display3", "value" : r}
+			data = json.dumps(data)
+			ws.write_message(data)			
+
+class SocketHandler(tornado.websocket.WebSocketHandler):
+	def check_origin(self, origin):
+		return True
+
+	def open(self):
+		global ws
+		ws=self
+		print "Websocket opened"
+
+	# Gestione dei messaggi in ricezione dal Chromium
+
+	def on_message(self, message):
+		#print message
+		data=json.loads(message)
+		
+		if data["event"]=="click":
+			if data["id"]=="abs_inc": 
+				if data["value"]=="ABS":
+					data = {"target": "abs_inc", "value" : "INC"}
+					print "ABS"
+				else:
+					data = {"target": "abs_inc", "value" : "ABS"}
+					print "INC"
+
+				data = json.dumps(data)
+				ws.write_message(data)
+				return
+
+		if data["event"]=="click":
+			if data["id"]=="mm_inch": 
+				if data["value"]=="MM":
+					data = {"target": "mm_inch", "value" : "INCH"}
+					print "INCH"
+				else:
+					data = {"target": "mm_inch", "value" : "MM"}
+					print "MM"
+
+				data = json.dumps(data)
+				ws.write_message(data)
+				return
+
+		if data["event"]=="click":
+			if data["id"]=="button_units": 
+				if data["value"]=="images/mm.jpg":
+					data = {"target": "button_units", "value" : "images/inch.jpg"}
+					print "Unit inch"
+				else:
+					data = {"target": "button_units", "value" : "images/mm.jpg"}
+					print "Unit mm"
+
+				data = json.dumps(data)
+				ws.write_message(data)
+				return
+
+		if data["event"]=="click":
+			if data["id"]=="button_mode": 
+				if data["value"]=="images/abs.jpg":
+					data = {"target": "button_mode", "value" : "images/inc.jpg"}
+					print "Mode inc"
+				else:
+					data = {"target": "button_mode", "value" : "images/abs.jpg"}
+					print "Mode abs"
+
+				data = json.dumps(data)
+				ws.write_message(data)
+				return
+
+	
+	def on_close(self):
+		print "Websocket closed"
+
+application = tornado.web.Application([
+	(r'/ws', SocketHandler),
+	(r"/(.*)", tornado.web.StaticFileHandler, {"path": "../www","default_filename": "index.html"}),
+])
+
+try:
+	t = threading.Thread(target=counter)
+	t.start()
+
+	application.listen(80,"0.0.0.0")
+	tornado.ioloop.IOLoop.instance().start()
+	
+except:
+	print("Unexpected error:", sys.exc_info()[0])
+	t_exit=True
+	t.join()
---
+++ www/js/main.js
@@ -0,0 +1,130 @@
+var host = window.location.host;
+var ws = new WebSocket('ws://'+host+'/ws');
+
+var display1;
+var display2;
+var display3;
+
+ws.onopen = function(){
+	console.log("Websocket opened");
+};
+
+// Gestione messaggi in arrivo da Python
+
+ws.onmessage = function(ev){
+	data=JSON.parse(ev.data);
+	
+	if (data.target=="display1") {
+		display1.setValue(data.value.toString());
+	}	
+
+	if (data.target=="display2") {
+		display2.setValue(data.value.toString());
+	}	
+	
+	if (data.target=="display3") {
+		display3.setValue(data.value.toString());
+	}	
+
+	if (data.target=="abs_inc") {
+		$("#abs_inc").text(data.value.toString());
+	}	
+
+	if (data.target=="mm_inch") {
+		$("#mm_inch").text(data.value.toString());
+	}	
+
+	if (data.target=="button_units") {
+		$("#button_units").attr("src",data.value.toString());
+	}	
+	
+	if (data.target=="button_mode") {
+		$("#button_mode").attr("src",data.value.toString());
+	}	
+};
+
+ws.onclose = function(ev){
+	console.log("Websocket closed");
+};
+
+ws.onerror = function(ev){
+	console.log("Websocket error");
+};
+
+ 
+$(document).ready(function() {
+	display1 = new SegmentDisplay("display1");
+
+	display1.pattern         = "###.#";
+	display1.displayAngle    = 6.5;
+	display1.digitHeight     = 32;
+	display1.digitWidth      = 17.5;
+	display1.digitDistance   = 3.1;
+	display1.segmentWidth    = 2.8;
+	display1.segmentDistance = 0.4;
+	display1.segmentCount    = 7;
+	display1.cornerType      = 3;
+	display1.colorOn         = "#ff330f";
+	display1.colorOff        = "#100505";
+
+	display1.setValue("0");
+
+
+	display2= new SegmentDisplay("display2");
+
+	display2.pattern         = " ##.#";
+	display2.displayAngle    = 6.5;
+	display2.digitHeight     = 32;
+	display2.digitWidth      = 17.5;
+	display2.digitDistance   = 3.1;
+	display2.segmentWidth    = 2.8;
+	display2.segmentDistance = 0.4;
+	display2.segmentCount    = 7;
+	display2.cornerType      = 3;
+	display2.colorOn         = "#ff330f";
+	display2.colorOff        = "#100505";
+
+	display2.setValue("0");
+	
+	display3= new SegmentDisplay("display3");
+
+	display3.pattern         = " ##.#";
+	display3.displayAngle    = 6.5;
+	display3.digitHeight     = 32;
+	display3.digitWidth      = 17.5;
+	display3.digitDistance   = 3.1;
+	display3.segmentWidth    = 2.8;
+	display3.segmentDistance = 0.4;
+	display3.segmentCount    = 7;
+	display3.cornerType      = 3;
+	display3.colorOn         = "#ff330f";
+	display3.colorOff        = "#100505";
+
+	display3.setValue("0");	
+	
+
+	$("#abs_inc").click(function(){
+		data={"event":"click","id":"abs_inc", "value" :$("#abs_inc").text()};
+		a=JSON.stringify(data);
+		ws.send(a);
+	});
+
+	$("#mm_inch").click(function(){
+		data={"event":"click","id": "mm_inch","value" : $("#mm_inch").text()};
+		a=JSON.stringify(data);
+		ws.send(a);
+	}); 
+
+	$("#button_units").click(function(){
+		data={"event":"click","id":"button_units", "value" :$("#button_units").attr("src")};
+		a=JSON.stringify(data);
+		ws.send(a);
+	});
+
+	$("#button_mode").click(function(){
+		data={"event":"click","id": "button_mode","value" : $("#button_mode").attr("src")};
+		a=JSON.stringify(data);
+		ws.send(a);
+	}); 
+
+});
